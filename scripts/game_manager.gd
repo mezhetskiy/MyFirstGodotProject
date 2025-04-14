5a7c96118bd116f9a8b6364a94439d4120d0552a
Mezhetskiy <79711895+mezhetskiy@users.noreply.github.com>
2025-04-14 18:22:43 +0300

    Update game_manager.gd

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,6 +4,6 @@ var score = 0
 @onready var score_label: Label = $ScoreLabel
 
 func add_point():
-	score += 1
+	score += 2
 	score_label.text = "You collected " + str(score) + " coins"
 	print("Test message")
