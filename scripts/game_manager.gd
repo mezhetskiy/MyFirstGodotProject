fd08b2eef383286082103168938e5491b95f4094
mezhetskiy <mezhetsky@gmail.com>
2025-04-14 18:27:00 +0300

    add test var

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,8 +2,9 @@ extends Node
 
 var score = 0 
 @onready var score_label: Label = $ScoreLabel
+var add_score : float = 4
 
 func add_point():
-	score += 1
+	score += 1 * add_score
 	score_label.text = "You collected " + str(score) + " coins"
 	print("Test message")
